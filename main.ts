2a8da1bceb97d0f1948976d16c8761268cecf087
nathoulix <58994177+nathoulix@users.noreply.github.com>
2021-05-03 16:29:41 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    if (n == 0) {
+    	
+    } else {
+        n += -10
+        envoi()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (n != 180) {
+        n += 10
+        envoi()
+    } else {
+    	
+    }
+})
+function envoi () {
+    pins.servoWritePin(AnalogPin.P1, n)
+}
+let n = 0
+n = 90
+pins.servoWritePin(AnalogPin.P1, n)
 basic.forever(function () {
-	
+    basic.showNumber(n)
 })
